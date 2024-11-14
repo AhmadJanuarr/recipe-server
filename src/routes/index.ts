dcc2415f19f287dcbe8983ead4b99934cc03b617
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-14 22:01:43 +0700

    feat : add the route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,15 @@ import express from "express";
 import { Signup } from "../controllers/Signup.controller";
 import { Login } from "../controllers/Login.controller";
 import { validateLogin, validateSignup } from "../utils/validation/auth";
+import { CreateRecipe, DeleteRecipe, GetRecipes, UpdateRecipe, GetRecipeById } from "../controllers/Recipes.controller";
 const router = express.Router();
 
 router.get("/admin/users",);
 router.post("/login", validateLogin, Login);
 router.post("/signup", validateSignup, Signup);
-router.get("/recipes",)
+router.get("/recipes", GetRecipes);
+router.get("/recipes/:id", GetRecipeById);
+router.post("/recipes", CreateRecipe);
+router.put("/recipes/:id", UpdateRecipe);
+router.delete("/recipes/:id", DeleteRecipe);
 export default router
