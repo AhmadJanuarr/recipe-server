75f74f11f4b8557f7d58fc1e25a68b087f6ce96b
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-16 06:58:10 +0700

    refactor code

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.sendStatus(401).json({
+        return res.status(401).json({
             success: false,
             message: "Unauthorized"
         })
@@ -11,9 +12,9 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
 
     jwt.verify(authHeader, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
         if (err) {
-            return res.sendStatus(403).json({
+            return res.status(401).json({
                 success: false,
-                message: "Forbidden"
+                message: "invalid token"
             })
         }
         next();
