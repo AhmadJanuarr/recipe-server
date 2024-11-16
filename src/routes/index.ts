e03b9b31cff972fc58e1e59f441cca81a54192e5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-16 15:02:48 +0700

    refactor : refactor the routes to make them look neat

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,24 @@
 import express from "express";
-import { Signup } from "../controllers/Signup.controller";
+import { Register } from "../controllers/Register.controller";
 import { Login } from "../controllers/Login.controller";
-import { validateLogin, validateSignup } from "../utils/validation/auth";
+import { validateLogin, validateRegister } from "../utils/validation/auth";
 import { CreateRecipe, DeleteRecipe, GetRecipes, UpdateRecipe, GetRecipeById } from "../controllers/Recipes.controller";
+import { LogoutUser } from "../controllers/User.controller";
 const router = express.Router();
 
+// auth routes: 
+router.post("/auth/login", validateLogin, Login);
+router.post("/auth/register", validateRegister, Register);
+router.post("/auth/logout", LogoutUser);
+
+// auth admin routes
 router.get("/admin/users",);
-router.post("/login", validateLogin, Login);
-router.post("/signup", validateSignup, Signup);
+
+// recipe routes
 router.get("/recipes", GetRecipes);
 router.get("/recipes/:id", GetRecipeById);
 router.post("/recipes", CreateRecipe);
 router.put("/recipes/:id", UpdateRecipe);
+
 router.delete("/recipes/:id", DeleteRecipe);
 export default router
