4d96ba78eeb792fc53bf43ab5208236a931e5935
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-03 22:28:36 +0700

    feat: add refresh token route and update user routes for better management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,16 +3,18 @@ import { Register } from "../controllers/Register.controller";
 import { Login } from "../controllers/Login.controller";
 import { validateLogin, validateRegister } from "../utils/validation/auth";
 import { CreateRecipe, DeleteRecipe, GetRecipes, UpdateRecipe, GetRecipeById } from "../controllers/Recipes.controller";
-import { LogoutUser } from "../controllers/User.controller";
+import { CreateUser, GetUsers, LogoutUser, RefreshToken } from "../controllers/User.controller";
+import { isAuthenticated } from "../middlewares/protected.route";
 const router = express.Router();
 
 // auth routes: 
 router.post("/auth/login", validateLogin, Login);
 router.post("/auth/register", validateRegister, Register);
 router.post("/auth/logout", LogoutUser);
+router.post('/auth/refreshToken', RefreshToken)
+// router.get("/profile", isAuthenticated, ProtectedRouteUser);
 
 // auth admin routes
-router.get("/admin/users",);
 
 // recipe routes 
 router.get("/recipes", GetRecipes);
