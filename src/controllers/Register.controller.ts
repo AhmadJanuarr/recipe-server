3e82903c5f8e65cf9309ae0e4049b17b5f9ebfbd
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-19 09:37:07 +0700

    feat: improve user registration logic with enhanced error messages and response structure

--- src/controllers/Register.controller.ts
+++ src/controllers/Register.controller.ts
@@ -1,50 +1,47 @@
-import { prisma } from '../../prisma/client/prisma';
+import { prisma } from "../../prisma/client/prisma";
 import { Request, Response } from "express";
 import { CreateUserByEmailAndPassword } from "../services/users.services";
 import { GenerateTokens } from "../middlewares/jwt";
 import { AddRefreshTokenToWhitelist } from "../services/auth.services";
 
-// controller untuk membuat user baru
 export const Register = async (req: Request, res: Response): Promise<void> => {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-        res.status(400).send({
-            success: false,
-            message: "Nama, email, dan kata sandi diperlukan"
-        });
-        return;
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400).json({
+      success: false,
+      message: "Nama, email, dan password diperlukan",
+    });
+    return;
+  }
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUser) {
+      res.status(400).json({
+        success: false,
+        message: "Email sudah digunakan, silahkan gunakan email lain",
+      });
+      return;
     }
-
-    // 
-    try {
-        const existingUser = await prisma.user.findUnique({
-            where: { email: email }
-        });
-        if (existingUser) {
-            res.status(400).send({
-                success: false,
-                message: "Email sudah digunakan"
-            });
-            return;
-        }
-
-        const user = await CreateUserByEmailAndPassword({name , email, password});
-        const  {accessToken, refreshToken} = await GenerateTokens(user);
-        await AddRefreshTokenToWhitelist({refreshToken, userId: user.id});
-        res.status(201).send({
-            success: true,
-            message: "User created successfully",   
-            data: user,
-            accessToken,
-            refreshToken,
-        });
-
-    } catch (error) {
-        console.error("Signup error:", error);
-        res.status(500).send({
-            success: false,
-            message: "Kesalahan Server Internal"
-        });
-    }
+    const user = await CreateUserByEmailAndPassword({ name, email, password });
+    const { password: _, ...userWithoutPassword } = user;
+    const { accessToken, refreshToken } = await GenerateTokens(user);
+    await AddRefreshTokenToWhitelist({ refreshToken, userId: user.id });
+    res.status(201).json({
+      success: true,
+      message: "User berhasil dibuat",
+      data: userWithoutPassword,
+      accessToken,
+      refreshToken,
+    });
+    return;
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Kesalahan Server Internal",
+    });
+    return;
+  }
 };
