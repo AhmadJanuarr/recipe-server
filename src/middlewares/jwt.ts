2a4e2498f81022197aa3ed403a301332a69e7eec
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-19 09:37:50 +0700

    refactor: improve type definitions for JWT token generation functions

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -1,39 +1,40 @@
 import jwt from "jsonwebtoken";
 import { prisma } from "../../prisma/client/prisma";
 
-// GenerateAccessToken function//
-export const GenerateAccessToken = (user :any ) => {
-    const secretAccess = process.env.JWT_ACCESS_SECRET
-    const  expireIn = 60 * 60 * 1;
-    if (!secretAccess) {
-        throw new Error("Secret Access is not defined");
-    }
-    return jwt.sign({userId: user.id, role: user.role}, secretAccess, {
-        expiresIn:  expireIn 
-    });
-}
+type UserProps = {
+  id: number;
+  role: string;
+};
+export const GenerateAccessToken = (user: UserProps) => {
+  const secretAccess = process.env.JWT_ACCESS_SECRET;
+  const expireIn = 60 * 60 * 1;
+  if (!secretAccess) {
+    throw new Error("Secret Access is not defined");
+  }
+  return jwt.sign({ userId: user.id, role: user.role }, secretAccess, {
+    expiresIn: expireIn,
+  });
+};
 
-// GenerateRefreshToken function
-export const GenerateRefreshToken = async(user : any) =>{
-    const secretRefresh = process.env.JWT_REFRESH_SECRET
-    if(!secretRefresh){
-        throw new Error("Secret Refresh is not defined");
-    }
-    const refreshToken= jwt.sign({userId: user.id}, secretRefresh, {expiresIn: "7d"});
+export const GenerateRefreshToken = async (user: UserProps) => {
+  const secretRefresh = process.env.JWT_REFRESH_SECRET;
+  if (!secretRefresh) {
+    throw new Error("Secret Refresh is not defined");
+  }
+  const refreshToken = jwt.sign({ userId: user.id }, secretRefresh, { expiresIn: "7d" });
 
-    await prisma.refreshToken.create({
-        data : {
-            hashToken : refreshToken,
-            userId : user.id,
-            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
-        }
-    })
-    return refreshToken
-}
+  await prisma.refreshToken.create({
+    data: {
+      hashToken: refreshToken,
+      userId: user.id,
+      expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    },
+  });
+  return refreshToken;
+};
 
-// GenerateTokens function//
-export const GenerateTokens =   async (user : any) =>{
-    const accessToken = GenerateAccessToken(user);
-    const refreshToken = await GenerateRefreshToken(user);
-    return{ accessToken, refreshToken }
-}
+export const GenerateTokens = async (user: any) => {
+  const accessToken = GenerateAccessToken(user);
+  const refreshToken = await GenerateRefreshToken(user);
+  return { accessToken, refreshToken };
+};
