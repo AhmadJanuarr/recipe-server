51ddd4afcfbc7cfbd250e489b4fd192ca537d2ee
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-19 09:39:52 +0700

    refactor: update user service functions with improved type definitions and consistent formatting

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,26 +1,31 @@
-import { prisma } from "../../prisma/client/prisma"
-import  bcrypt  from 'bcrypt';
+import { prisma } from "../../prisma/client/prisma";
+import bcrypt from "bcrypt";
 
-export const FindUserByEmail = (email : string) => {
-    return  prisma.user.findUnique({
-        where: {email}
-    })
-}
+type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+};
 
-export const FindUserById =(id :number) =>{
-    return prisma.user.findUnique({
-        where: {id}
-    })
-}
+export const FindUserByEmail = (email: string) => {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+};
 
-export const CreateUserByEmailAndPassword = (user : any) =>{
-    user.password = bcrypt.hashSync(user.password, 10)
-    return prisma.user.create({
-    data:  {
-        ...user,
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-    
-})
-}
+export const FindUserById = (id: number) => {
+  return prisma.user.findUnique({
+    where: { id },
+  });
+};
+
+export const CreateUserByEmailAndPassword = (user: UserProps) => {
+  user.password = bcrypt.hashSync(user.password, 10);
+  return prisma.user.create({
+    data: {
+      ...user,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+};
