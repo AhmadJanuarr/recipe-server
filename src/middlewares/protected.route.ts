9775f298cc811eff56faf2de90ae3bb88fdfb829
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-19 09:38:58 +0700

    refactor: enhance authentication middleware with custom request interface and improved error handling

--- src/middlewares/protected.route.ts
+++ src/middlewares/protected.route.ts
@@ -1,7 +1,13 @@
 import  jwt from 'jsonwebtoken';
-import {  Request, Response } from "express"
+import {  NextFunction, Request, Response } from "express"
 
-export const isAuthenticated = (req : Request, res : Response)  => {
+interface CustomRequest extends Request {
+  payload?: {
+    userId: number;
+    role: string;
+  };
+}
+export const isAuthenticated = (req : CustomRequest, res : Response , next : NextFunction)  => {
     const {authorization} = req.headers
 
     // mengecek apakah ada token
@@ -15,13 +21,14 @@ export const isAuthenticated = (req : Request, res : Response)  => {
 
     try {
         const token = authorization.split(' ')[1];
-        const decode = jwt.verify(token, process.env.JWT_ACCESS_SECRET || 'secret') as {userId : string};
-        req.payload = {userId : decode.userId};   
+        const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET || 'secret') as {userId : number, role : string};
+        req.payload = payload;
+        next();
       } catch (err) {
-        res.status(401);
-        if ((err as Error).name === 'TokenExpiredError') {
-          throw new Error((err as Error).name);
-        }
-        throw new Error('Un-Authorized');  
+        res.status(401).json({
+          success : false,
+          message : "Invalid token"
+        })
+        return
       }
     }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,24 +2,34 @@ import express from "express";
 import { Register } from "../controllers/Register.controller";
 import { Login } from "../controllers/Login.controller";
 import { validateLogin, validateRegister } from "../utils/validation/auth";
-import { CreateRecipe, DeleteRecipe, GetRecipes, UpdateRecipe, GetRecipeById } from "../controllers/Recipes.controller";
-import { CreateUser, GetUsers, LogoutUser, RefreshToken } from "../controllers/User.controller";
+import {
+  CreateRecipe,
+  DeleteRecipe,
+  GetRecipes,
+  UpdateRecipe,
+  GetRecipeByName,
+} from "../controllers/Recipes.controller";
+import { LogoutUser } from "../controllers/User.controller";
 import { isAuthenticated } from "../middlewares/protected.route";
+import { RefreshAccessToken } from "../controllers/Token.controller";
+import { authorizeRoles } from "../middlewares/authorize.roles";
 const router = express.Router();
 
-// auth routes: 
+// auth routes:
 router.post("/auth/login", validateLogin, Login);
 router.post("/auth/register", validateRegister, Register);
 router.post("/auth/logout", LogoutUser);
-router.post('/auth/refreshToken', RefreshToken)
+router.post("/auth/refreshToken", RefreshAccessToken);
 // router.get("/profile", isAuthenticated, ProtectedRouteUser);
 
-// auth admin routes
-
-// recipe routes 
+// Route accessible to Admin only
+router.get("/admin/data", isAuthenticated, authorizeRoles(["ADMIN"]), (req, res) => {
+  res.json({ message: "Hello Admin!" });
+});
+// recipe routes
 router.get("/recipes", GetRecipes);
-router.get("/recipes/:id", GetRecipeById);
+router.get("/recipes/:recipeName", GetRecipeByName);
 router.post("/recipes", CreateRecipe);
 router.put("/recipes/:id", UpdateRecipe);
 router.delete("/recipes/:id", DeleteRecipe);
-export default router 
+export default router;
