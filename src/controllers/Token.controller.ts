8a53cb3e20c8ae946ffde33138b90d7c415663c3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-19 09:37:21 +0700

    feat: add Token controller for refreshing and revoking access tokens with error handling

---
+++ src/controllers/Token.controller.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import { FindRefreshToken, RevokeTokens } from "../services/auth.services";
+import { FindUserById } from "../services/users.services";
+import { GenerateTokens } from "../middlewares/jwt";
+import { AddRefreshTokenToWhitelist, DeleteRefreshTokenById } from "../services/auth.services";
+export const RefreshAccessToken = async (req : Request, res : Response) =>{
+    const { refreshToken } = req.body;
+    if(!refreshToken){
+            res.status(400).json({
+                success: false,
+                message: "Token tidak ditemukan"
+            });
+            return;
+        }
+    try{
+       
+        const SavedRefreshToken = await FindRefreshToken(refreshToken);
+        if(!SavedRefreshToken || SavedRefreshToken.revoked ===true  || Date.now() >= SavedRefreshToken.expiresAt.getTime()){
+            res.status(404).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+        const user = await FindUserById(SavedRefreshToken!.userId);
+        if(!user) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        await DeleteRefreshTokenById(SavedRefreshToken!.id)  
+        const {accessToken, refreshToken : newRefreshToken} =  await GenerateTokens(user);
+        await AddRefreshTokenToWhitelist({refreshToken : newRefreshToken, userId: user!.id});
+
+        res.status(200).json({
+            success: true,
+            message: "Token refreshed",
+            data: {
+                accessToken,
+                refreshToken : newRefreshToken
+            }
+        })
+    }
+    catch(error: any){
+        res.status(500).json({
+            success: false,
+            message: "Gagal refresh token",
+            msg: error.message
+        });
+    }
+}
+
+export const RevokeRefreshTokens = async (req : Request, res : Response) =>{
+    try{
+        const {userId} = req.body;
+        await RevokeTokens(userId);
+        res.status(200).json({
+            status: true,
+            message: "Tokens revoked"
+        })
+    }catch(error : any) {
+        res.status(500).json({
+            success: false,
+            message: "Gagal revoke token",
+            msg: error.message
+        });
+    }
+}
