af6b41442028036054c32c6daadac3d753ae9e2e
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-21 22:37:35 +0700

    refactor: remove dotenv configuration from Supabase config file

--- src/config/supabase.config.ts
+++ src/config/supabase.config.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
+
 const SUPABASE_URL = process.env.SUPABASE_URL as string;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
 
