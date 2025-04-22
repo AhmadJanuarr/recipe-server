ec10fb50979fdb7faa700685e85d2e7c7aedb5e5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 03:06:44 +0700

    feat: add UploadImageAvatarToSupabase function for avatar uploads and rename image upload function for recipes

--- src/services/supabase.services.ts
+++ src/services/supabase.services.ts
@@ -1,10 +1,22 @@
 import { Supabase } from "../config/supabase.config";
 
-export const UploadImageToSupabase = async (file: Express.Multer.File) => {
+export const UploadImageRecipeToSupabase = async (file: Express.Multer.File) => {
   const fileName = `${Date.now()}-${file.originalname}`;
+  if (!file) return console.log("file not found");
   const { data, error } = await Supabase.storage.from("images").upload(fileName, file.buffer);
   if (error) {
     throw new Error(error.message);
   }
   return `${process.env.SUPABASE_URL}/storage/v1/object/public/images/${fileName}`;
+};
+
+export const UploadImageAvatarToSupabase = async (file: Express.Multer.File, userId: number) => {
+  if (!file) return console.log("file not found");
+  const fileName = `${Date.now()}-${file.originalname}`;
+  const filePath = `${userId}/${fileName}`;
+  const { data, error } = await Supabase.storage.from("avatars").upload(filePath, file.buffer);
+  if (error) {
+    throw new Error(error.message);
+  }
+  return `${process.env.SUPABASE_URL}/storage/v1/object/public/avatars/${userId}/${fileName}`;
 };
