779b80802436cfe61455bc47924d2e5c35139f9e
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-26 22:33:15 +0700

    fix: adjust middleware usage for recipe routes in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -29,9 +29,9 @@ router.get("/admin/data", isAuthenticated, authorizeRoles(["ADMIN"]), (req, res)
   res.json({ message: "Hello Admin!" });
 });
 // recipe routes
-router.get("/recipes", upload.single("image"), GetRecipes);
+router.get("/recipes", GetRecipes);
 router.get("/recipes/:recipeName", GetRecipeByName);
-router.post("/recipes", CreateRecipe);
+router.post("/recipes", upload.single("image"), CreateRecipe);
 router.put("/recipes/:id", UpdateRecipe);
 router.delete("/recipes/:id", DeleteRecipe);
 
